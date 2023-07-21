dd6506fb77839405444e8ecd0b5b3da48e1088aa
hiesa1 <hiesa1@naver.com>
2023-07-21 20:17:51 +0900

    0721 깃허브 연습하기

---
+++ test10_11_12/src/main/java/com/example/test10_11_12/GitActivity.kt
@@ -0,0 +1,11 @@
+package com.example.test10_11_12
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GitActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_git)
+    }
+}
